7be1118a4eafd220d9b838010b59efc64f2246c0
fangqin <18222604921@163.com>
2020-06-01 21:39:26 +0800

    split images

--- utils/split_img.py
+++ utils/split_img.py
@@ -1,18 +1,24 @@
 import os
 from PIL import Image
 
-def crop_img(image,img_name,im_type="img"):
+def crop_img(image,img_name,im_type="img",split_num=2):
 	if im_type=="img":
 		save_path="/mnt/city/split_train_img/"
 	else:
 		save_path="/mnt/city/split_train_anno/"
 	w,h = image.size  
-	box1 = (0,0,w//2,h//2) #左上角建系，需要给出对角的两个点的坐标 
-	box2 = (w//2,0,w,h//2) 
-	box3 = (0,h//2,w//2,h)
-	box4 = (w//2,h//2,w,h)
-	#im1是上半部分 #im2是下半部分 
-	for num,box in enumerate([box1,box2,box3,box4]):
+	if split_num==4:
+		box1 = (0,0,w//2,h//2) #左上角建系，需要给出对角的两个点的坐标 
+		box2 = (w//2,0,w,h//2) 
+		box3 = (0,h//2,w//2,h)
+		box4 = (w//2,h//2,w,h)
+		boxs = [box1,box2,box3,box4]
+	elif split_num==2:
+		box1 = (0,0,w//2,h)
+		box2 = (w//2,0,w,h)
+		boxs = [box1,box2]
+
+	for num,box in enumerate(boxs):
 		im = image.crop(box)
 		img_name_list = img_name.split(".")
 		pre_save_path=os.path.join(save_path,img_name_list[0]+"_"+str(num+1)+"."+img_name_list[1])
@@ -31,16 +37,16 @@ def get_txt(img_folder,anno_folder):
 	print("txt gene done!")
 
 if __name__=="__main__":
-	#	with open("../txt/city_train.txt","r") as f:
-	#		lines = [line.strip().split(" ") for line in f]
-	#		img_path_list=[lines[i][0] for i in range(len(lines))]
-	#		label_path_list=[lines[i][1] for i in range(len(lines))]
-	#
-	#	i=0
-	#	for img_path,label_path in zip(img_path_list,label_path_list):
-	#		i+=1
-	#		image = Image.open(img_path) 
-	#		crop_img(image,img_path.split("/")[-1])
-	#		label= Image.open(label_path) 
-	#		crop_img(label,label_path.split("/")[-1],im_type="anno")
+	with open("../txt/city_train.txt","r") as f:
+		lines = [line.strip().split(" ") for line in f]
+		img_path_list=[lines[i][0] for i in range(len(lines))]
+		label_path_list=[lines[i][1] for i in range(len(lines))]
+
+	i=0
+	for img_path,label_path in zip(img_path_list,label_path_list):
+		i+=1
+		image = Image.open(img_path) 
+		crop_img(image,img_path.split("/")[-1])
+		label= Image.open(label_path) 
+		crop_img(label,label_path.split("/")[-1],im_type="anno")
 	get_txt("/mnt/city/split_train_img/","/mnt/city/split_train_anno/" )
