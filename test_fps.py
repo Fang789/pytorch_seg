50335499753c3709ebb497eacf1f436decf34740
fangqin <18222604921@163.com>
2020-06-09 18:38:06 +0800

    add parameters calculate

--- test_fps.py
+++ test_fps.py
@@ -11,10 +11,13 @@ if __name__ == "__main__":
 
 	model = RetinaSeg(backbone,classes=n_classes)
 	model.load_state_dict(torch.load(model_path) )
+	num_parameters = sum(torch.numel(parameter) for parameter in model.parameters())
+	print("model parameters:{:.2f}M".format(num_parameters/(10**6)))
+
 	model.cuda()
 	model.eval()
 	x = torch.Tensor(1, 3, 360,480).cuda()
-	N = 1000 #test pics numbers
+	N = 100 #test pics numbers
 	with torch.no_grad():
 		torch.cuda.synchronize()
 		st = time.time()*1000
