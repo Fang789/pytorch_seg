cb404ffefe861516792ff8482c3e7b7695dd11f0
fangqin <18222604921@163.com>
2020-06-10 11:04:31 +0800

    fix bug

--- get_flops.py
+++ get_flops.py
@@ -1,15 +1,18 @@
 from model.retina import RetinaSeg
 import torch
-from torchstat import stat
-
-def get_flops(net):
-	stat(net,(3,360,480))
+from ptflops import get_model_complexity_info
+#from torchstat import stat
 
 if __name__=="__main__":
 	n_classes=12
-	backbone="Resnet50"
+	backbone="efficient"
 	model_path="./weights/camvid_best_model.pth"
 
-	net = RetinaSeg(backbone,classes=n_classes)
-	net.load_state_dict(torch.load(model_path) )
-	get_flops(net)
+	with torch.cuda.device(0):
+		net = RetinaSeg(backbone,classes=n_classes)
+		net.load_state_dict(torch.load(model_path) )
+		macs, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True,
+												print_per_layer_stat=True, verbose=True)
+		print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+		print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+		#flops=2*macs
