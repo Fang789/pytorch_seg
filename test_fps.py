8a37be68f5e261d017b85a1dbf3d94092cde88ba
fangqin <18222604921@163.com>
2020-06-10 09:58:52 +0800

    update

--- test_fps.py
+++ test_fps.py
@@ -3,20 +3,13 @@ import torch
 import numpy as np
 from model.retina import RetinaSeg
 
-if __name__ == "__main__":
-
-	n_classes=12
-	backbone="efficient"
-	model_path="./weights/camvid_best_model.pth"
-
-	model = RetinaSeg(backbone,classes=n_classes)
-	model.load_state_dict(torch.load(model_path) )
+def get_parameter(model):
 	num_parameters = sum(torch.numel(parameter) for parameter in model.parameters())
 	print("model parameters:{:.2f}M".format(num_parameters/(10**6)))
 
+def get_fps(model,x):
 	model.cuda()
 	model.eval()
-	x = torch.Tensor(1, 3, 360,480).cuda()
 	N = 100 #test pics numbers
 	with torch.no_grad():
 		torch.cuda.synchronize()
@@ -28,3 +21,18 @@ if __name__ == "__main__":
 			torch.cuda.synchronize()
 		end=time.time()*1000
 		print(N*1000/(end-st))
+
+if __name__ == "__main__":
+
+	n_classes=12
+	backbone="Resnet50"
+	model_path="./weights/camvid_best_model.pth"
+
+	model = RetinaSeg(backbone,classes=n_classes)
+	model.load_state_dict(torch.load(model_path) )
+
+	x = torch.Tensor(1, 3, 360,480).cuda()
+	get_parameter(model)
+	get_fps(model,x)
+	
+
