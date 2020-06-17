13dcb671b51555fcfd9c626999c8113f89d21ce5
fangqin <18222604921@163.com>
2020-06-17 21:03:06 +0800

    add distributed load

--- visible.py
+++ visible.py
@@ -11,11 +11,13 @@ from utils.label_color import read_labelcolors
 from utils.helper import get_confusion_matrix,get_data_class
 from loss.losses import CategoricalFocalLoss
 from torch.nn import functional as F
+#from PIL import Image
 import cv2
 import argparse
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from apex.parallel import convert_syncbn_model
 
 # helper function for data visualization
 def visualize(img_name,**images):
@@ -35,58 +37,65 @@ def visualize(img_name,**images):
 
 def visible(args):
 
-	CLASSES = get_data_class(args.data_name)
-	n_classes = len(CLASSES)+1
+	classes = get_data_class(args.data_name)
+	n_classes = 1 if classes ==1 else classes+1
 	# load best saved checkpoint
-	model = RetinaSeg(args.backbone,classes=n_classes)
-	model.load_state_dict(torch.load(args.model_path) )
+	if args.train_style =="distribute":
+		checkpoint = torch.load(args.model_path)
+		model = convert_syncbn_model(RetinaSeg(args.backbone,classes=n_classes))
+		model.load_state_dict(checkpoint['model'])
+	else:
+		model = RetinaSeg(args.backbone,classes=n_classes)
+		model.load_state_dict(torch.load(args.model_path) )
+
 	model = model.cuda()
 	model.eval()
 
 	#Visualize predictions
 	test_dataset = Dataset(
 			args.test_txt, 
-			classes = CLASSES, 
+			n_classes = classes, 
 			height = args.height,
 			width = args.width,
-			resize = False,
+			resize = True,
 	)
 
 	test_dataset_vis = Dataset(
 			args.test_txt, 
-			classes = CLASSES, 
+			n_classes = classes, 
 			height = args.height,
 			width = args.width,
-			preprocessing = False,
-			resize = False,
+			preprocessing=False,
+			resize = True,
 	)
 
-	for i in range(5):
+	for i in range(3):
 		n = np.random.choice(len(test_dataset))
 		img_name = str(i)+'.png'
 		
-		image_vis = test_dataset_vis[n][0].astype('uint8')
+		image_vis = np.array(test_dataset_vis[n][0]).astype('uint8')	
 		image, gt_mask = test_dataset[n] #(3,360,480) (12,360,480)
 		
-		x_tensor = torch.from_numpy(image).cuda().unsqueeze(0)
+		x_tensor = image.cuda().unsqueeze(0)
 		with torch.no_grad():	
 			pr_mask = model(x_tensor)
-			pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+			pr_mask = pr_mask.squeeze().cpu().numpy().round()
 
 		visualize(
 			img_name=img_name,
-			image=image_vis, 
-			ground_truth_mask=gt_mask.argmax(axis=0), 
+			image = image_vis, 
+			ground_truth_mask=np.array(gt_mask), 
 			predicted_mask=pr_mask.argmax(axis=0)
 		)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--backbone', type=str,default='efficient')
-	parser.add_argument('--height', type=int, default=360)
-	parser.add_argument('--width', type=int, default=480)
+	parser.add_argument('--height', type=int, default=512)
+	parser.add_argument('--width', type=int, default=1024)
 	parser.add_argument('--test_txt', type=str, default='./txt/camvid_val.txt')
 	parser.add_argument('--model_path', type=str, default='./weights/camvid_best_model.pth')
+	parser.add_argument('--train_style', type=str, default='distribute')
 	parser.add_argument('--data_name', type=str, default='camvid',
 						help='Dataset to use',
 						choices=['ade20k','city','voc','camvid'])
