eb4acee774fc149fd95af4d4d521dc47eb3e6aea
fangqin <18222604921@163.com>
2020-06-17 21:12:27 +0800

    convert camvid to 11 class

--- data/deal_img.py
+++ data/deal_img.py
@@ -2,7 +2,25 @@ import os
 import shutil
 import cv2
 import numpy as np
-from skimage.io import imread, imsave
+from PIL import Image
+import numpy as np
+
+def convert_camvid_to11(anno_convered_path):
+	anno_folder = "/mnt/Camvid/labels/"
+	annos_list = os.listdir(anno_folder)
+	label_mapping={21:0,4:1,8:2,17:3,19:4,26:5,20:6,9:7,5:8,16:9,2:10,31:1,1:1,3:1,12:1,28:3,18:3,15:4,10:4,
+			11:4,23:6,24:6,29:7,22:8,6:8,25:8,27:8,7:9,0:9,14:10,13:10,30:11}
+	for i,anno_name in enumerate(annos_list):
+		mask_path = os.path.join(anno_folder,anno_name)
+		mask = Image.open(mask_path)
+		mask = np.array(mask)
+		mask_copy = mask.copy()
+		for k, v in label_mapping.items():
+			mask_copy[mask == k] = v
+		mask = Image.fromarray(mask_copy.astype(np.uint8))
+		save_path = os.path.join(anno_convered_path,anno_name)
+		mask.save(save_path)
+		print("have save {}/{}".format(i,len(annos_list)),flush=True,end="\r")
 
 def split_camvid_data():
 
@@ -11,7 +29,7 @@ def split_camvid_data():
 			for line in fr:
 				img_path=line.split(" ")[0]
 				img_name=img_path.split("/")[-1]
-				anno_name = img_name.split(".")[0]+"_L.png"
+				anno_name = img_name.split(".")[0]+"_P.png"
 
 				img_folder=os.path.join(dest_path,filename[1])
 				anno_folder=os.path.join(dest_path,filename[2])
@@ -46,51 +64,38 @@ def get_camvid_txt():
 				line=img+" "+anno+"\n"
 				fw.write(line)
 
-def convert_rgb2mask(arr_3d):
-	"""
-		for camvid
-	"""
+def get_ade20k_txt():
 
-	Sky = (128, 128, 128)
-	Building = (128, 0, 0)
-	Pole = (192, 192, 128)
-	Road_marking = (255, 69, 0)
-	Road = (128, 64, 128)
-	Pavement = (60, 40, 222)
-	Tree = (128, 128, 0)
-	SignSymbol = (192, 128, 128)
-	Fence = (64, 64, 128)
-	Car = (64, 0, 128)
-	Pedestrian = (64, 64, 0)
-	Bicyclist = (0, 128, 192)
-
-	label_codes = [Sky,Building,Pole,Road_marking,Road,Pavement,Tree,SignSymbol,Fence,Car,Pedestrian,Bicyclist]
-	palette = {k:v for k,v in enumerate(label_codes)}
-	arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
-
-	for i,c in palette.items():
-		m = np.all(arr_3d == np.array(c).reshape(1, 1, 3), axis=2)
-		arr_2d[m] = i
-
-	return arr_2d
-
-def predeal_label():
-	anno_list = os.listdir(src_anno_path)
-	for anno_name in anno_list:
-		anno_path = os.path.join(src_anno_path,anno_name)
-		anno = imread(anno_path)
-		if (len(anno.shape) > 2):
-			anno = convert_rgb2mask(anno)
-			save_path = os.path.join("/mnt/Camvid/mask_all",anno_name)
-			imsave(save_path,anno)
+	img_folder="/mnt/ADEChallengeData2016/images/"
+	anno_folder="/mnt/ADEChallengeData2016/annotations/"
+	save_folder="/home/fangqin/progect/pytorch_seg/txt"
+	for data in ("training","validation"):
+		img_path=os.path.join(img_folder,data)
+		anno_path=os.path.join(anno_folder,data)
+		img_list=sorted(os.listdir(img_path))
+		anno_list=sorted(os.listdir(anno_path))
+		if data=="training":
+			save_path=os.path.join(save_folder,"ade20k_train.txt")
+		elif data=="validation":
+			save_path =os.path.join(save_folder,"ade20k_val.txt")
+		with open(save_path,"w") as fw:
+			for img,anno in zip(img_list,anno_list):
+				img_full_path=os.path.join(img_path,img)
+				anno_full_path=os.path.join(anno_path,anno)
+				line = img_full_path+" "+anno_full_path+"\n"
+				fw.write(line)
 
 if __name__=="__main__":
-
-	src_img_path='/mnt/Camvid/camvid_all_img'
-	src_anno_path='/mnt/Camvid/camvid_all_anno'
+	
+	anno_convered_path = "/mnt/Camvid/annos"
+	if not os.path.exists(anno_convered_path):
+		os.mkdir(anno_convered_path)
+	convert_camvid_to11(anno_convered_path)
+	src_img_path='/mnt/Camvid/images'
+	src_anno_path="/mnt/Camvid/annos"
 	dest_path='/mnt/Camvid/'
 
-	#predeal_label()
 	split_camvid_data()
 	get_camvid_txt()
+	#get_ade20k_txt()
 
