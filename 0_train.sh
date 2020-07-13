63d20360a778fc4e9e197119696bdead78006bce
fangqin <18222604921@163.com>
2020-07-13 17:04:15 +0800

    update

--- 0_train.sh
+++ 0_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-python tmp.py \
+python train.py \
 	--backbone 'efficient' \
 	--input_height 480 \
 	--input_width 360 \
@@ -8,6 +8,6 @@ python tmp.py \
 	--batch_size 2 \
 	--datadir './txt/' \
 	--weights './weights/' \
-	--lr 2e-4 \
+	--lr 1e-4 \
 	--data_name 'camvid' \
 	--gpu_id '0,1'
