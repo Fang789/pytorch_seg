a65246c07dd95a4ae6b4cf2789057a155b0bf14b
fangqin <18222604921@163.com>
2020-07-13 16:59:03 +0800

    add distribute

--- get_flops.py
+++ get_flops.py
@@ -1,17 +1,26 @@
 from model.retina import RetinaSeg
 import torch
 from ptflops import get_model_complexity_info
+from apex import amp
+from apex.parallel import convert_syncbn_model
 
 if __name__=="__main__":
 	n_classes=12
-	backbone="Resnest50"
-	model_path="/home/fangqin/progect/pytorch_seg_z/weights/camvid_resnest_aspp_64.pth"
+	backbone="efficient"
+	model_path="./weights/camvid_best_model_dist_73_7.pth"
+	checkpoint = torch.load(model_path)
 
+	dist = True
 	with torch.cuda.device(0):
-		net = RetinaSeg(backbone,classes=n_classes)
-		net.load_state_dict(torch.load(model_path) )
-		macs, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True,
-												print_per_layer_stat=True, verbose=True)
+		if dist:
+			model = convert_syncbn_model(RetinaSeg(backbone,classes=n_classes,aux=False))
+		else:
+			model = RetinaSeg(backbone,classes=n_classes)
+
+		model.load_state_dict(checkpoint['model'])
+
+		macs, params = get_model_complexity_info(model, (3, 360,480), as_strings=True,
+												print_per_layer_stat=False, verbose=True)
 		print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
 		print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 		#FLOPs=2*MACs #1GFLOPs=10**9FLOPs
